d1de7cc11d0f9a7f3df96c183f4c272bdee7ffba
Merwann <merwannheret@gmail.com>
2025-09-04 15:07:35 +0200

    update: button home page

---
+++ Adopte.js
@@ -0,0 +1,112 @@
+const searchButton = document.getElementById("searchButton");
+const animalType = document.getElementById("animal_type");
+const resultSearch = document.getElementById("resultSearch");
+
+let db = null;
+
+async function getOrInitDB() {
+    if (db === null) {
+        const SQL = await initSqlJs({
+            locateFile: (file) =>
+                `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.13.0/${file}`,
+        });
+
+        const response = await fetch("SQL/adaopte.db");
+        const buffer = await response.arrayBuffer();
+        db = new SQL.Database(new Uint8Array(buffer));
+        return db;
+    } else {
+        return db;
+    }
+}
+
+async function getAnimals(id) {
+    const db = await getOrInitDB();
+    const res = db.exec(`SELECT * FROM animal WHERE animal_id = ${id};`);
+    return res[0].values[0];
+}
+
+async function idToAnimalsType(idAnimalType) {
+    const db = await getOrInitDB();
+    const res = db.exec(
+        `SELECT name FROM animal_type WHERE animal_type_id = ${idAnimalType};`
+    );
+    return res[0].values[0];
+}
+
+async function animalsTypeToId(animalType) {
+    const db = await getOrInitDB();
+    const res = db.exec(
+        `SELECT animal_type_id FROM animal_type WHERE name = "${animalType}";`
+    );
+    return res[0].values[0];
+}
+
+async function idToAnimalsBreed(idAnimalBreed) {
+    const db = await getOrInitDB();
+    const res = db.exec(
+        `SELECT name FROM breed WHERE breed_id = ${idAnimalBreed};`
+    );
+    return res[0].values[0];
+}
+
+async function getAnimalsType() {
+    const db = await getOrInitDB();
+    const res = db.exec(`SELECT name FROM animal_type;`);
+    return res[0].values;
+}
+
+async function displayAnimalsType() {
+    let animalTypeList = await getAnimalsType();
+    for (let i = 0; i < animalTypeList.length; i++) {
+        const option = document.createElement("option");
+        option.innerText = animalTypeList[i][0];
+        animalType.appendChild(option);
+    }
+}
+displayAnimalsType();
+
+async function displayCardAnimals(id) {
+    let animalInfo = await getAnimals(id);
+    let animalType = await idToAnimalsType(animalInfo[3]);
+    let animalBreed = await idToAnimalsBreed(animalInfo[4]);
+
+    const div = document.createElement("div");
+    div.className = "basis-sm shadow-[0_0_20px_rgba(0,0,0,0.1)] rounded-lg";
+    div.innerHTML = `<img class="h-70 w-full object-cover object-[50%_50%]" src=${animalInfo[8]} alt="">
+      <div class="p-10">
+      <p class="mb-3 text-lg">
+        ${animalType}
+      </p>
+      <h1 class="font-bold text-[#8482FF] text-xl mb-3">${animalInfo[1]}</h1>
+      <p class="mb-1">${animalBreed} • ${animalInfo[2]}</p>
+      <p class="mb-6">${animalInfo[5]}</p>
+      <p class="mb-6">${animalInfo[7]}</p>
+      <button class="mb-3 bg-[#333333] text-white font-semibold px-6 py-4 rounded-full shadow active:bg-[#4d4c4b] transition">Rencontrer</button>
+    `;
+    resultSearch.appendChild(div);
+}
+
+async function search() {
+    resultSearch.innerHTML = null;
+    if (animalType.value === "Tous les animaux") {
+        for (let i = 1; i <= 8; i++) displayCardAnimals(i);
+    } else {
+        let id = await animalsTypeToId(animalType.value);
+        let resultSearchId = await searchByAnimalTypeId(id);
+        for (let i = 0; i < resultSearchId.length; i++) {
+            displayCardAnimals(resultSearchId[i]);
+        }
+    }
+}
+
+
+async function searchByAnimalTypeId(id) {
+    const db = await getOrInitDB();
+    const res = db.exec(
+        `SELECT animal_id FROM animal WHERE animal_type_id = ${id};`
+    );
+    return res[0].values;
+}
+
+searchButton.addEventListener("click", search);
---
+++ HomePage.js
@@ -0,0 +1,11 @@
+const searchButton = document.getElementById("searchButton");
+const allAnimals = document.getElementById("allAnimals");
+
+
+searchButton.addEventListener("click", ()=>{
+    window.location.href = "Adopte.html";
+});
+
+allAnimals.addEventListener("click", ()=>{
+    window.location.href = "Adopte.html";
+});
