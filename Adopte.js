ad269686c70354e2b62c001bdfe979b326ff41b0
Merwann <merwannheret@gmail.com>
2025-09-04 15:29:27 +0200

    fix: searchByCity

--- Adopte.js
+++ Adopte.js
@@ -1,6 +1,7 @@
 const searchButton = document.getElementById("searchButton");
 const animalType = document.getElementById("animal_type");
 const resultSearch = document.getElementById("resultSearch");
+let locationInput = document.getElementById("location");
 
 let db = null;
 
@@ -89,17 +90,31 @@ async function displayCardAnimals(id) {
 
 async function search() {
     resultSearch.innerHTML = null;
-    if (animalType.value === "Tous les animaux") {
-        for (let i = 1; i <= 8; i++) displayCardAnimals(i);
+    if (locationInput.value == "") {
+        if (animalType.value === "Tous les animaux") {
+            for (let i = 1; i <= 10; i++) displayCardAnimals(i);
+        } else {
+            let id = await animalsTypeToId(animalType.value);
+            let resultSearchId = await searchByAnimalTypeId(id);
+            for (let i = 0; i < resultSearchId.length; i++) {
+                displayCardAnimals(resultSearchId[i]);
+            }
+        }
     } else {
-        let id = await animalsTypeToId(animalType.value);
-        let resultSearchId = await searchByAnimalTypeId(id);
-        for (let i = 0; i < resultSearchId.length; i++) {
-            displayCardAnimals(resultSearchId[i]);
+        if (animalType.value == "Tous les animaux") {
+            let resultSearchByCity = await searchByCity(locationInput.value);
+            for (let i = 0; i < resultSearchByCity.length; i++) {
+                displayCardAnimals(resultSearchByCity[i]);
+            }
+        } else {
+            let id = await animalsTypeToId(animalType.value);
+            let resultSearchIdAndCity = await searchByAnimalTypeIdAndCity(id, locationInput.value);
+            for (let i = 0; i < resultSearchIdAndCity.length; i++) {
+                displayCardAnimals(resultSearchIdAndCity[i]);
+            }
         }
     }
 }
-
 
 async function searchByAnimalTypeId(id) {
     const db = await getOrInitDB();
@@ -109,4 +124,19 @@ async function searchByAnimalTypeId(id) {
     return res[0].values;
 }
 
+async function searchByCity(city) {
+    const db = await getOrInitDB();
+    const res = db.exec(`SELECT animal_id FROM animal WHERE city = "${city}";`);
+    return res[0].values;
+}
+
+async function searchByAnimalTypeIdAndCity(id, city) {
+    const db = await getOrInitDB();
+    const res = db.exec(
+        `SELECT animal_id FROM animal WHERE city = "${city}" AND animal_type_id = ${id};`
+    );
+    return res[0].values;
+}
+
 searchButton.addEventListener("click", search);
+
--- HomePage.js
+++ HomePage.js
@@ -1,6 +1,8 @@
 const searchButton = document.getElementById("searchButton");
 const allAnimals = document.getElementById("allAnimals");
+const animalType = document.getElementById("animal_type");
 
+let db = null;
 
 searchButton.addEventListener("click", ()=>{
     window.location.href = "Adopte.html";
@@ -9,3 +11,37 @@ searchButton.addEventListener("click", ()=>{
 allAnimals.addEventListener("click", ()=>{
     window.location.href = "Adopte.html";
 });
+
+
+async function getOrInitDB() {
+    if (db === null) {
+        const SQL = await initSqlJs({
+            locateFile: (file) =>
+                `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.13.0/${file}`,
+        });
+
+        const response = await fetch("SQL/adaopte.db");
+        const buffer = await response.arrayBuffer();
+        db = new SQL.Database(new Uint8Array(buffer));
+        return db;
+    } else {
+        return db;
+    }
+}
+
+async function getAnimalsType() {
+    const db = await getOrInitDB();
+    const res = db.exec(`SELECT name FROM animal_type;`);
+    return res[0].values;
+}
+
+async function displayAnimalsType() {
+    let animalTypeList = await getAnimalsType();
+    for (let i = 0; i < animalTypeList.length; i++) {
+        const option = document.createElement("option");
+        option.innerText = animalTypeList[i][0];
+        animalType.appendChild(option);
+    }
+}
+
+displayAnimalsType();
