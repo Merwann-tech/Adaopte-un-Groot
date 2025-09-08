4bcfc6b7baa7475a3ad40dfd98a95bde88998f4e
Hellyes31 <elies.auguet@gmail.com>
2025-09-08 11:12:06 +0200

    add: images update: adaopte.db and animals.sql

--- SQL/animals.sql
+++ SQL/animals.sql
@@ -57,7 +57,7 @@ INSERT INTO breed(name, animal_type_id) VALUES
 ('Falabella', '6');
 
 INSERT INTO animal(name, age, breed_type_id, animal_type_id, city, zipcode, description, image) VALUES
-('Touklakos', '12 ans', '11','6','Nantes', '44100','Touklakos adore galoper dans les hautes herbes.', 'images/Petoche.jpg');
+('Touklakos', '12 ans', '11','6','Nantes', '44100','Touklakos adore galoper dans les hautes herbes.', 'images/Touklakos.jpg');
 
 -- Ajout Patapouf
 
@@ -68,24 +68,24 @@ INSERT INTO breed(name, animal_type_id) VALUES
 ('Panda Roux', '7');
 
 INSERT INTO animal(name, age, breed_type_id, animal_type_id, city, zipcode, description, image) VALUES
-('Patapouf', '2 ans', '12','7','Nantes', '44200','Patapouf est chou comme tout mais il a une tête à connaître le goût des feutres.', 'images/Petoche.jpg');
+('Patapouf', '2 ans', '12','7','Nantes', '44200','Patapouf est chou comme tout mais il a une tête à connaître le goût des feutres.', 'images/Patapouf.jpg');
 
 -- Ajout PLEIN DANIMAUX
 
 INSERT INTO animal (name, age, breed_type_id, animal_type_id, city, zipcode, description, image) VALUES
-('Ajax ', '1 an', '13', '11', 'Nantes', '44000', 'Charlie est un petit chien calme et affectueux qui adore les balades tranquilles et les caresses sur le canapé.', 'images/charlesdeluvio-K4mSJ7kc0As-unsplash.jpg'),
-('Balourd', '48 ans', '14', '8', 'Nantes', '44000', 'Mia est une boule de tendresse curieuse et joueuse, toujours prête à ronronner près de vous.', 'images/manja-vitolic-gKXKBY-C-Dk-unsplash.jpg'),
-('Kumo', '3 ans', '15', '11', 'Nantes', '44000', 'Coco est un petit lapin plein d’énergie, très doux et parfait pour un foyer aimant et paisible.', 'images/chan-swan-NKyl19P5IHg-unsplash.jpg'),
-('Boursemol', '4 ans', '16', '11', 'Nantes', '44000', 'Rex est un chien loyal et majestueux, idéal pour une personne douce et patiente qui aime les câlins silencieux.', 'images/alan-king-KZv7w34tluA-unsplash.jpg'),
-('Malinx', '7 ans', '17', '11', 'Nantes', '44000', 'Luna adore les coins ensoleillés et les siestes à vos côtés. Elle vous offrira tout l’amour d’un regard félin.', 'images/jae-park-7GX5aICb5i4-unsplash.jpg'),
-('Nemo', '3 mois', '18', '9', 'Nantes', '44000', 'Biscuit est tout petit mais plein de vie ! Il aime les légumes croquants et les instants de douceur en famille.', 'images/yosei-g-OVgE3m4MHKM-unsplash.jpg'),
-('Sushi', '5 mois', '19', '9', 'Nantes', '44000', 'Rio est discret, mignon comme tout, et adore explorer les petits coins de la maison avec délicatesse.', 'images/melanie-kreutz-IFnknR2Mv5o-unsplash.jpg'),
-('Milktooth', '2 ans', '20', '9', 'Nantes', '44000', 'Ruby est une boule de neige pleine d’amour ! Elle adore les promenades et les câlins par temps frais.', 'images/peri-stojnic-5Vr_RVPfbMI-unsplash.jpg'),
-('Soap', '4 ans', '16', '11', 'Nantes', '44000', 'Rex est un chien loyal et majestueux, idéal pour une personne douce et patiente qui aime les câlins silencieux.', 'images/alan-king-KZv7w34tluA-unsplash.jpg'),
-('Balou', '7 ans', '17', '11', 'Nantes', '44000', 'Luna adore les coins ensoleillés et les siestes à vos côtés. Elle vous offrira tout l’amour d’un regard félin.', 'images/jae-park-7GX5aICb5i4-unsplash.jpg'),
-('Marin', '3 mois', '18', '9', 'Nantes', '44000', 'Biscuit est tout petit mais plein de vie ! Il aime les légumes croquants et les instants de douceur en famille.', 'images/yosei-g-OVgE3m4MHKM-unsplash.jpg'),
-('Eagly', '5 mois', '19', '9', 'Nantes', '44000', 'Rio est discret, mignon comme tout, et adore explorer les petits coins de la maison avec délicatesse.', 'images/melanie-kreutz-IFnknR2Mv5o-unsplash.jpg'),
-('Cormoch', '2 ans', '20', '9', 'Nantes', '44000', 'Ruby est une boule de neige pleine d’amour ! Elle adore les promenades et les câlins par temps frais.', 'images/peri-stojnic-5Vr_RVPfbMI-unsplash.jpg');
+('Ajax ', '1 an', '13', '11', 'Nantes', '44000', 'Ajax est un jeune lion curieux et plein d’énergie, toujours prêt à bondir dans de nouvelles aventures.', 'images/Ajax.jpg'),
+('Balourd', '48 ans', '14', '8', 'Nantes', '44000', 'Balourd est un sage protecteur des glaces, calme et impressionnant, mais avec un grand cœur tendre.', 'images/Balourd.jpg'),
+('Kumo', '3 ans', '15', '11', 'Nantes', '44000', 'Kumo est un tigre élégant et joueur, qui adore courir et montrer sa force avec douceur.', 'images/Kumo.jpg'),
+('Boursemol', '4 ans', '16', '11', 'Nantes', '44000', 'Boursemol est un félin discret et agile, toujours en quête d’un coin tranquille pour se reposer.', 'images/Boursemol.jpg'),
+('Malinx', '7 ans', '17', '11', 'Nantes', '44000', 'Malinx a l’œil vif et le pas léger, un compagnon mystérieux qui cache beaucoup d’affection derrière son allure sauvage.', 'images/Malinx.jpg'),
+('Nemo', '3 mois', '18', '9', 'Nantes', '44000', 'Nemo est un petit nageur joyeux et coloré, toujours en train d’explorer son univers marin.', 'images/Nemo.jpg'),
+('Sushi', '5 mois', '19', '9', 'Nantes', '44000', 'Sushi est vif et insouciant, il aime glisser dans les courants avec légèreté et malice.', 'images/Sushi.jpg'),
+('Milktooth', '2 ans', '20', '9', 'Nantes', '44000', 'Milktooth est jeune mais déjà impressionnant, un explorateur des océans au regard curieux et joueur.', 'images/Milktooth.jpg'),
+('Soap', '4 ans', '16', '9', 'Nantes', '44000', 'Soap est souple et discret, un véritable petit mystère des profondeurs, doux à observer.', 'images/Soap.jpg'),
+('Balou', '7 ans', '17', '8', 'Nantes', '44000', 'Balou est affectueux et gourmand, il adore les siestes au soleil et les moments paisibles.', 'images/Balou.jpg'),
+('Marin', '3 mois', '18', '9', 'Nantes', '44000', 'Marin est minuscule et éclatant, il nage avec insouciance et apporte une touche de joie à chaque regard.', 'images/Marin.jpg'),
+('Eagly', '5 mois', '19', '10', 'Nantes', '44000', 'Eagly est jeune mais majestueux, toujours prêt à prendre son envol vers de nouveaux horizons.', 'images/Eagly.jpg'),
+('Cormoch', '2 ans', '20', '10', 'Nantes', '44000', 'Ce corbeau est malin et observateur, avec un caractère curieux et une élégance discrète.', 'images/corbeau.jpg');
 
 
 INSERT INTO animal_type (name) VALUES
